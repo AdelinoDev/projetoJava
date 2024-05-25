7d4c0a41da7f9de98d938023bd0f052a1c9a4c05
AdelinoNetto <adelino.mecanica@gmail.com>
2024-05-24 22:40:28 -0300

    repositorio referente as interface DepartmentDao e SellerDao

--- proejtoDao/src/application/Program.java
+++ proejtoDao/src/application/Program.java
@@ -11,19 +11,11 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department departmet = new Department(1, "Books");
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, departmet);
-		
-		System.out.println(departmet);
-		System.err.println();
+		Department obj = new Department(1, "Books");
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
 		System.out.println(seller);
-		
-		/*
-		 Connection conn = DB.getConnection(); // conectando com o banco
-		DB.closeConnection(); //desconectando com o banco
-		
-		System.out.println("Banco conectado com suscesso!!");
-		*/
+
 	}
 
 }
